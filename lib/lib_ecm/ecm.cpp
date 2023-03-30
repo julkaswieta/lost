a4a79c075decc301eda946858c86cceb8a35df83
Julia Swietochowska <julkaswietochowska@gmail.com>
2023-03-30 12:52:13 +0200

    clean up ecm library

--- lib/lib_ecm/ecm.cpp
+++ lib/lib_ecm/ecm.cpp
@@ -2,129 +2,141 @@
 
 using namespace std;
 
+// Entity
+
 Entity::Entity(Scene* const s)
-    : _position({0, 0}), _rotation(0), _alive(true), _visible(true),
-      scene(s), _fordeletion(false) {}
+    : position({0, 0}), rotation(0), alive(true), visible(true),
+      scene(s), forDeletion(false) {}
 
-void Entity::addTag(const std::string& t) { _tags.insert(t); }
-const std::set<std::string>& Entity::getTags() const { return _tags; }
 
-void Entity::update(double dt) {
-  if (!_alive) {
-    return;
-  }
-  for (size_t i = 0; i < _components.size(); i++) {
-    if (_components[i]->is_fordeletion()) {
-      _components.erase(_components.begin() + i);
-      --i;
+void Entity::Update(double dt) {
+    if (!alive) {
+        return;
     }
-    _components[i]->update(dt);
-  }
+    for (size_t i = 0; i < components.size(); i++) {
+        if (components[i]->isForDeletion()) {
+            components.erase(components.begin() + i);
+            --i;
+        }
+        components[i]->Update(dt);
+    }
 }
 
-bool Entity::is_fordeletion() const { return _fordeletion; }
-
-void Entity::render() {
-  if (!_visible) {
-    return;
-  }
-  for (auto& c : _components) {
-    c->render();
-  }
+void Entity::Render() {
+    if (!visible)
+        return;
+    for (auto& c : components)
+        c->Render();
 }
 
-const sf::Vector2f& Entity::getPosition() const { return _position; }
+void Entity::addTag(const std::string& t) { tags.insert(t); }
+
+const std::set<std::string>& Entity::getTags() const { return tags; }
+
+const sf::Vector2f& Entity::getPosition() const { return position; }
 
 void Entity::setPosition(const sf::Vector2f& _position) {
-  Entity::_position = _position;
+  Entity::position = _position;
 }
 
-float Entity::getRotation() const { return _rotation; }
+float Entity::getRotation() const { return rotation; }
 
-void Entity::setRotation(float _rotation) { Entity::_rotation = _rotation; }
+void Entity::setRotation(float _rotation) { Entity::rotation = _rotation; }
 
-bool Entity::isAlive() const { return _alive; }
+bool Entity::isAlive() const { return alive; }
 
-void Entity::setAlive(bool _alive) { Entity::_alive = _alive; }
+void Entity::setAlive(bool _alive) { Entity::alive = _alive; }
+
+bool Entity::isForDeletion() const { return forDeletion; }
 
 void Entity::setForDelete() {
-  _fordeletion = true;
-  _alive = false;
-  _visible = false;
+  forDeletion = true;
+  alive = false;
+  visible = false;
 }
 
-bool Entity::isVisible() const { return _visible; }
+bool Entity::isVisible() const { return visible; }
 
-void Entity::setVisible(bool _visible) { Entity::_visible = _visible; }
-
-Component::Component(Entity* const p) : _parent(p), _fordeletion(false) {}
+void Entity::setVisible(bool _visible) { Entity::visible = _visible; }
 
 Entity::~Entity() {
-  // Components can inter-depend on each other, so deleting them may take
-  // multiple passes. We Keep deleting components until we can't delete any
-  // more
-  int deli = 0;
-  while (deli != _components.size()) {
-    deli = _components.size();
-    _components.erase(
-        remove_if(_components.begin(), _components.end(),
-                  [](auto& sp) { return (sp.use_count() <= 1); }),
-        _components.end());
-  }
+    // Components can inter-depend on each other, so deleting them may take
+    // multiple passes. We Keep deleting components until we can't delete any
+    // more
+    int deli = 0;
+    while (deli != components.size()) {
+        deli = components.size();
+        components.erase(
+            remove_if(components.begin(), components.end(),
+                [](auto& sp) { return (sp.use_count() <= 1); }),
+            components.end());
+    }
 
-  if (_components.size() > 0) {
-    throw std::runtime_error(
-        "Can't delete entity, someone is grabbing a component!");
-  }
+    if (components.size() > 0) {
+        throw std::runtime_error(
+            "Can't delete entity, someone is grabbing a component!");
+    }
 
-  _components.clear();
+    components.clear();
 }
+
+
+// Component
+
+Component::Component(Entity* const p) : parent(p), forDeletion(false) {}
+
+bool Component::isForDeletion() const { return forDeletion; }
 
 Component::~Component() {}
 
-bool Component::is_fordeletion() const { return _fordeletion; }
 
-void EntityManager::update(double dt) {
-  for (size_t i = 0; i < list.size(); i++) {
-    if (list[i]->is_fordeletion()) {
-      list.erase(list.begin() + i);
-      --i;
-      continue;
+// EntityManager
+
+// update all entities in the manager
+void EntityManager::Update(double dt) {
+    for (size_t i = 0; i < list.size(); i++) {
+        if (list[i]->isForDeletion()) {
+            list.erase(list.begin() + i);
+            --i;
+            continue;
+        }
+        if (list[i]->alive) {
+            list[i]->Update(dt);
+        }
     }
-    if (list[i]->_alive) {
-      list[i]->update(dt);
-    }
-  }
 }
 
-void EntityManager::render() {
-  for (auto& e : list) {
-    if (e->_visible) {
-      e->render();
+// render all entities in the manager
+void EntityManager::Render() {
+    for (auto& e : list) {
+        if (e->visible) {
+            e->Render();
+        }
     }
-  }
 }
 
+// find all entities with a specific tag in the maanager
 vector<shared_ptr<Entity>> EntityManager::find(const string& tag) const {
-  vector<shared_ptr<Entity>> ret;
-  for (auto& e : list) {
-    const auto tgs = e->_tags;
-    if (tgs.find(tag) != tgs.end()) {
-      ret.push_back(e);
+    vector<shared_ptr<Entity>> ret;
+    for (auto& e : list) {
+        const auto tgs = e->tags;
+        if (tgs.find(tag) != tgs.end()) {
+            ret.push_back(e);
+        }
     }
-  }
-  return ret;
+    return ret;
 }
 
+// find all entities with a group of specific tags 
 vector<shared_ptr<Entity>>
 EntityManager::find(const vector<string>& tags) const {
-  vector<shared_ptr<Entity>> ret;
-  for (auto& e : list) {
-    const auto tgs = e->_tags;
-    if (any_of(tags.begin(), tags.end(),
-               [&tgs](auto t) { return tgs.find(t) != tgs.end(); })) {
-      ret.push_back(e);
+    vector<shared_ptr<Entity>> ret;
+    for (auto& e : list) {
+        const auto tgs = e->tags;
+        if (any_of(tags.begin(), tags.end(),
+            [&tgs](auto t) { return tgs.find(t) != tgs.end(); })) {
+            ret.push_back(e);
+        }
     }
-  }
-  return ret;
+    return ret;
 }
