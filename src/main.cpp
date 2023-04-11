3127a41d83775632fa3436634c47a05ffd9b32ba
Julia Swietochowska <julkaswietochowska@gmail.com>
2023-04-11 15:44:12 +0200

    create controls menu layout

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include "engine.h"
 #include "game.h"
-#include "scenes/scene_menu.h"
 
 using namespace std;
 
 MenuScene menu;
 Level1Scene level1;
 SettingsScene settings;
+ControlsScene controls;
 
 int main()
 {
-	Engine::Start(1280, 720, "Lost", &menu);
+	Engine::Start(1280, 720, "Lost", &controls);
 }
--- src/scenes/scene_controls.h
+++ src/scenes/scene_controls.h
@@ -8,4 +8,18 @@ public:
 	void Load() override;
 	void Unload() override;
 	void Update(const double& dt) override;
+
+private:
+	const int OPTIONS_COUNT = 8;
+	const int TOP_MARGIN = 50;
+	const int LEFT_MARGIN = 200;
+
+	std::vector<std::shared_ptr<Entity>> options;
+	int selectedOptionIndex;
+
+	void moveUp();
+	void moveDown();
+	void executeSelectedOption();
+
+	sf::Vector2f positionOptionInWindow(int i);
 };
