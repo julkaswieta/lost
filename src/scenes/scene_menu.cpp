df68297484632f595243a924dafcf21610610d61
Dillon Aitken <dillonaitken87@gmail.com>
2023-04-25 22:32:59 +0100

    Got menu level loading. Activated settings trigger and level 1 trigger. Temporarily made exit triggers solid to avoid player escaping level and crashing the game.

--- src/scenes/scene_menu.cpp
+++ src/scenes/scene_menu.cpp
@@ -1,82 +1,126 @@
 #include "scene_menu.h"
+#include "../components/cmp_hurt_player.h"
+#include "../components/cmp_player_physics.h"
+#include "../components/cmp_sprite.h"
 #include "../components/cmp_text.h"
 #include "../game.h"
-#include <SFML/Window/Keyboard.hpp>
+#include <LevelSystem.h>
 #include <iostream>
+#include <thread>
 #include "../controls.h"
 
 using namespace std;
 using namespace sf;
 
+static shared_ptr<Entity> player;
 
 void MenuScene::Load() {
-    selectedOptionIndex = -1;
+    cout << " Menu Scene Load" << endl;
+    ls::LoadLevelFile("res/levels/menu_level.txt", 60.0f);
+
+    auto ho = Engine::getWindowSize().y - (ls::getHeight() * 60.f);
+    ls::setOffset(Vector2f(0, ho));
+
+    // Create player
     {
-        string optionsText[4] = { "LOST", "Start Game", "Settings", "Exit" };
-        for (int i = 0; i < 4; ++i) {
-            auto menuOption = makeEntity();
-            auto textCmp = menuOption->addComponent<TextComponent>(optionsText[i]);
-            textCmp->getText().setOrigin(Vector2(textCmp->getText().getLocalBounds().width * 0.5f, textCmp->getText().getLocalBounds().height * 0.5f));
-            textCmp->getText().setPosition(Vector2f(Engine::getWindowSize().x * 0.5f, TOP_MARGIN + ((i + 1) * 50)));
-            if(i > 0)
-                options.push_back(menuOption);
+        player = makeEntity();
+        player->setPosition(ls::getTilePosition(ls::findTiles(ls::START)[0]));
+        auto s = player->addComponent<ShapeComponent>();
+        s->setShape<sf::RectangleShape>(Vector2f(40.f, 60.f));
+        s->getShape().setFillColor(Color::Magenta);
+        s->getShape().setOrigin(Vector2f(20.f, 30.f));
+        player->addTag("player");
+        player->addComponent<PlayerPhysicsComponent>(Vector2f(40.f, 60.f));
+    }
+
+    // Add physics colliders to level tiles
+    {
+        auto walls = ls::findTiles(ls::WALL);
+        for (auto w : walls) {
+            auto pos = ls::getTilePosition(w);
+            pos += Vector2f(30.f, 30.f); //offset to center
+            auto e = makeEntity();
+            e->setPosition(pos);
+            e->addComponent<PhysicsComponent>(false, Vector2f(60.f, 60.f));
         }
     }
+
+    // Temporarily add physics colliders to exit tiles
+    {
+        auto exits = ls::findTiles(ls::EXIT);
+        for (auto w : exits) {
+            auto pos = ls::getTilePosition(w);
+            pos += Vector2f(30.f, 30.f); //offset to center
+            auto e = makeEntity();
+            e->setPosition(pos);
+            e->addComponent<PhysicsComponent>(false, Vector2f(60.f, 60.f));
+        }
+    }
+
+    // Add hurt components to hazard tiles
+    {
+        vector<sf::Vector2ul> hazards;
+
+        for (auto su : ls::findTiles(ls::SPIKE_UP))
+            hazards.push_back(su);
+
+        for (auto sd : ls::findTiles(ls::SPIKE_DOWN))
+            hazards.push_back(sd);
+
+        for (auto sr : ls::findTiles(ls::SPIKE_RIGHT))
+            hazards.push_back(sr);
+
+        for (auto sl : ls::findTiles(ls::SPIKE_LEFT))
+            hazards.push_back(sl);
+
+        for (auto sl : ls::findTiles(ls::SPIKE_BALL))
+            hazards.push_back(sl);
+
+        for (auto sl : ls::findTiles(ls::SAWBLADE))
+            hazards.push_back(sl);
+
+        for (auto s : hazards) {
+            auto h = makeEntity();
+            auto pos = ls::getTilePosition(s) + Vector2f(30.f, 30.f); // Offset to center
+            h->setPosition(pos);
+            h->addComponent<HurtComponent>();
+        }
+    }
+
+    //Simulate long loading times to check loading screen works
+    //std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    cout << " Menu Scene Load Done" << endl;
+
     setLoaded(true);
 }
 
-void MenuScene::Update(const double& dt) {
-    if (Keyboard::isKeyPressed(Controls::MenuDown)) {
-        moveDown();
-    }
-    if (Keyboard::isKeyPressed(Controls::MenuUp)) {
-        moveUp(); 
-    }
-    if (Keyboard::isKeyPressed(Controls::MenuSelect)) {
-        executeSelectedOption();
-    }
+void MenuScene::Unload() {
+    cout << "Menu Scene Unload" << endl;
+    player.reset();
+    ls::Unload();
+    Scene::Unload();
+}
 
+void MenuScene::Update(const double& dt) {
+    if (Keyboard::isKeyPressed(Controls::Exit)) {
+        //Engine::ChangeScene(())
+    }
+    if (ls::getTileAt(player->getPosition()) == ls::EXIT) {
+        //Exit game
+    }
+    else if (ls::getTileAt(player->getPosition()) == ls::SETTINGS) {
+        Engine::ChangeScene((Scene*)&settings);
+    }
+    else if (ls::getTileAt(player->getPosition()) == ls::LEVEL1) {
+        Engine::ChangeScene((Scene*)&level1);
+    }
+    else if (!player->isAlive()) {
+        Engine::ChangeScene((Scene*)&menu);
+    }
     Scene::Update(dt);
 }
 
-void MenuScene::moveUp() {
-    if (selectedOptionIndex - 1 >= 0) {
-        options[selectedOptionIndex]->getComponents<TextComponent>()[0]->getText().setFillColor(Color::White);
-        selectedOptionIndex--;
-        options[selectedOptionIndex]->getComponents<TextComponent>()[0]->getText().setFillColor(Color::Red);
-        std::this_thread::sleep_for(std::chrono::milliseconds(150)); // these are here so the cursor does not move too fast
-    }
-}
-
-void MenuScene::moveDown() {
-    // handle initial state when nothing is selected
-    if (selectedOptionIndex == -1) {
-        selectedOptionIndex = 0;
-        options[selectedOptionIndex]->getComponents<TextComponent>()[0]->SetColor(Color::Red);
-        std::this_thread::sleep_for(std::chrono::milliseconds(150)); // these are here so the cursor does not move too fast
-    }
-    else if (selectedOptionIndex + 1 < OPTIONS_COUNT) {
-        options[selectedOptionIndex]->getComponents<TextComponent>()[0]->SetColor(Color::White);
-        selectedOptionIndex++;
-        options[selectedOptionIndex]->getComponents<TextComponent>()[0]->SetColor(Color::Red);
-        std::this_thread::sleep_for(std::chrono::milliseconds(150)); // these are here so the cursor does not move too fast
-    }
-}
-
-void MenuScene::executeSelectedOption() {
-    switch (selectedOptionIndex) {
-    case 0:
-        Engine::ChangeScene(&level1);
-        break;
-    case 1:
-        Engine::ChangeScene(&settings);
-        break;
-    case 2:
-        Engine::getWindow().close();
-    }
-}
-
-void MenuScene::Unload() {
-    options.clear();
-    Scene::Unload();
+void MenuScene::Render() {
+    ls::Render(Engine::getWindow());
+    Scene::Render();
 }
--- src/scenes/scene_menu.h
+++ src/scenes/scene_menu.h
@@ -4,21 +4,11 @@
 
 class MenuScene : public Scene {
 public:
-	MenuScene() = default;
-	~MenuScene() override = default;
 	void Load() override;
+
 	void Unload() override;
+
 	void Update(const double& dt) override;
 
-private:
-	const int OPTIONS_COUNT = 3;
-	const int TOP_MARGIN = 100;
-
-	std::vector<std::shared_ptr<Entity>> options;
-	int selectedOptionIndex;
-
-	void moveUp();
-	void moveDown();
-
-	void executeSelectedOption();
+	void Render() override;
 };
